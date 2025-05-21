83e2e630ee7a49622a0a7f78be8909e96ac101b2
ralfspoeth <ralf.spoeth@gmail.com>
2025-05-21 17:46:05 +0200

    wip

--- src/main/java/io/github/ralfspoeth/jsontestsuite/Main.java
+++ src/main/java/io/github/ralfspoeth/jsontestsuite/Main.java
@@ -15,25 +15,30 @@ void main() throws Exception {
     var yMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:y*.json");
     var nMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:n*.json");
     var iMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:i*.json");
+    record Result(Element element, Throwable exception, Path p) {}
+    var results = new ArrayList<Result>();
     try (var sources = Files.list(resourcesDir)) {
         sources.filter(f -> jsonMatcher.matches(f.getFileName()))
                 .forEach(p -> {
                     try {
                         var result = parse(p);
                         if (nMatcher.matches(p.getFileName())) {
-                            System.out.printf("%s shouldn't have been parsed into %s%n", p.getFileName(), result);
+                            results.add(new Result(result, null, p));
+                            //System.err.printf("%s shouldn't have been parsed into %s%n", p.getFileName(), result);
                         } else {
-                            System.out.printf("%s successfully parsed into %s%n", p.getFileName(), result);
+                            //System.out.printf("%s successfully parsed into %s%n", p.getFileName(), result);
                         }
                     } catch (Throwable t) {
                         if (yMatcher.matches(p.getFileName())) {
-                            System.out.printf("%s should haven been successfully parsed; exception is: %s%n", p.getFileName(), t);
+                            results.add(new Result(null, t, p));
+                            //System.err.printf("%s should haven been successfully parsed; exception is: %s%n", p.getFileName(), t);
                         } else if (iMatcher.matches(p.getFileName())) {
-                            System.out.printf("Acceptable not to have parsed %s, reason being %s%n", p.getFileName(), t);
+                            //System.out.printf("Acceptable not to have parsed %s, reason being %s%n", p.getFileName(), t);
                         } else {
-                            System.out.printf("Good not to have parsed %s, reason being %s%n", p.getFileName(), t);
+                            //System.out.printf("Good not to have parsed %s, reason being %s%n", p.getFileName(), t);
                         }
                     }
                 });
+        results.forEach(System.out::println);
     }
 }
