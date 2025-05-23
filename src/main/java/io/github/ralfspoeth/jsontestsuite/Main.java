89375f6f5100feeabb69b4629d81d35bf986eaeb
Ralf Spöth <ralf.spoeth@profidata.com>
2025-05-23 10:18:24 +0200

    using 1.1.36, better reporting

--- src/main/java/io/github/ralfspoeth/jsontestsuite/Main.java
+++ src/main/java/io/github/ralfspoeth/jsontestsuite/Main.java
@@ -3,42 +3,78 @@ import io.github.ralfspoeth.json.io.JsonReader;
 
 import static java.util.Objects.requireNonNull;
 
-Element parse(Path p) throws Exception {
+record Result(Path p, Element element, Throwable exception) {}
+
+private final List<Result> acceptedNs = new ArrayList<>();
+private final List<Result> rejectedYs = new ArrayList<>();
+private final List<Result> acceptedIs = new ArrayList<>();
+private final List<Result> rejectedIs = new ArrayList<>();
+
+// parse a single JSON file
+Result parse(Path p) {
     try (var rdr = new JsonReader(Files.newBufferedReader(p, StandardCharsets.UTF_8))) {
-        return rdr.readElement();
+        return new Result(p, rdr.readElement(),null);
+    } catch (Throwable t) {
+        return new Result(p, null, t);
     }
+}
+
+Predicate<Path> fileNameFilter(FileSystem fs, String pattern) {
+    return p -> fs.getPathMatcher("glob:" + pattern).matches(p.getFileName());
+}
+
+void collectYs(Path resourceDir) throws IOException {
+    try(var files = Files.list(resourceDir)) {
+        files.filter(fileNameFilter(resourceDir.getFileSystem(), "y*.json"))
+                .map(this::parse)
+                .filter(r -> r.exception != null)
+                .forEach(rejectedYs::add);
+    }
+}
+
+void collectNs(Path resourceDir) throws IOException {
+    try(var files = Files.list(resourceDir)) {
+        files.filter(fileNameFilter(resourceDir.getFileSystem(), "n*.json"))
+                .map(this::parse)
+                .filter(r -> r.exception == null && r.element!=null)
+                .forEach(acceptedNs::add);
+    }
+}
+
+void collectIs(Path resourceDir) throws IOException {
+    try(var files = Files.list(resourceDir)) {
+        files.filter(fileNameFilter(resourceDir.getFileSystem(), "i*.json"))
+                .map(this::parse)
+                .forEach(r -> {
+                    if (r.exception == null) {
+                        acceptedIs.add(r);
+                    } else {
+                        rejectedIs.add(r);
+                    }
+                });
+    }
+}
+
+void listResults(List<Result> results) {
+    System.out.println("Path\tElement\tException");
+    results.forEach(r -> System.out.printf("%s\t%s\t%s%n",
+            r.p,
+            r.element==null?"":r.element.json().replace("\n", "\\n").replace("\r", "\\r").replace("\t", "\\t"),
+            r.exception==null?"":r.exception.getMessage())
+    );
 }
 
 void main() throws Exception {
     var resourcesDir = Path.of(requireNonNull(getClass().getResource("/")).toURI());
-    var jsonMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:*.json");
-    var yMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:y*.json");
-    var nMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:n*.json");
-    var iMatcher = resourcesDir.getFileSystem().getPathMatcher("glob:i*.json");
-    record Result(Path p, Element element, Throwable exception) {}
-    var results = new ArrayList<Result>();
-    try (var sources = Files.list(resourcesDir)) {
-        sources.filter(f -> jsonMatcher.matches(f.getFileName()))
-                .forEach(p -> {
-                    try {
-                        var elem = parse(p);
-                        if (nMatcher.matches(p.getFileName()) && elem!=null) {
-                            results.add(new Result(p, elem, null));
-                            //System.err.printf("%s shouldn't have been parsed into %s%n", p.getFileName(), result);
-                        } else {
-                            //System.out.printf("%s successfully parsed into %s%n", p.getFileName(), result);
-                        }
-                    } catch (Throwable t) {
-                        if (yMatcher.matches(p.getFileName())) {
-                            results.add(new Result(p, null, t));
-                            //System.err.printf("%s should haven been successfully parsed; exception is: %s%n", p.getFileName(), t);
-                        } else if (iMatcher.matches(p.getFileName())) {
-                            //System.out.printf("Acceptable not to have parsed %s, reason being %s%n", p.getFileName(), t);
-                        } else {
-                            //System.out.printf("Good not to have parsed %s, reason being %s%n", p.getFileName(), t);
-                        }
-                    }
-                });
-        results.forEach(System.out::println);
-    }
+    collectNs(resourcesDir);
+    collectYs(resourcesDir);
+    collectIs(resourcesDir);
+    System.out.printf("Accepted N Files: %d%n", acceptedNs.size());
+    listResults(acceptedNs);
+    System.out.printf("Rejected Y Files: %d%n", rejectedYs.size());
+    listResults(rejectedYs);
+    System.out.printf("Accepted I Files: %d%n", acceptedIs.size());
+    listResults(acceptedIs);
+    System.out.printf("Rejected I Files: %d%n", rejectedIs.size());
+    listResults(rejectedIs);
 }
